f77be18c70569108b68fb2122cc36c68a0d189e4
JoshuaKin <joshuakin@163.com>
2018-06-15 23:52:57 +0800

    a
    a

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var app = express();
 app.all('*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Credentials', true);
